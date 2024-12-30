1843b31e68e2b9e8c90bb0d8cf9d575cc559156e
Erik-wiklund <85eriwik@skola.boras.se>
2024-12-30 14:19:32 +0100

    Fixed display of image

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -32,9 +32,10 @@
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4">
             @foreach ($featuredShows as $show)
                 <div class="bg-gray-700 rounded-lg overflow-hidden shadow-lg">
-                    <img src="{{ $show->poster_url }}" alt="{{ $show->title }}" class="w-full h-56 object-cover">
-                    <div class="p-4">
-                        <h3 class="text-xl font-semibold">{{ $show->title }}</h3>
+                    <img class="p-4" src="{{ asset('images/' . $show->image) }}" alt="{{ $show->name }}"
+                        class="w-full h-56 object-cover">
+                    <div class="px-4 py-2">
+                        <h3 class="text-xl font-semibold text-center">{{ $show->name }}</h3>
                         <p class="text-sm">{{ $show->description }}</p>
                     </div>
                 </div>
